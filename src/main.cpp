14e3b27594376bb591ff68c481d5651aba1238bd
Andreas Bjørsvik <andreas.bjorsvik@gmail.com>
2022-02-07 17:42:30 +0100

    added new code

--- src/main.cpp
+++ src/main.cpp
@@ -13,21 +13,37 @@ void setup() {
   pinMode(LED_pin, OUTPUT);
   Serial.begin(9600);
   FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-  // put your setup code here, to run once:
+  LEDS.addLeds<WS2812B,DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(20);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 500);
 }
 
 void loop() {
-  // digitalWrite(LED_pin, HIGH);
-  // delay(500);
-  // digitalWrite(LED_pin, LOW);
-  // delay(500);
-  Serial.println("loop compleeted");
-  for (int i = 0; i < NUM_LEDS; i++){
-     leds[i] = CRGB::Blue;
-     FastLED.show();
-     delay(200);
-     leds[i] = CRGB::Black;
-     FastLED.show();
- }
+  // fill_gradient_RGB(leds, NUM_LEDS, CRGB::Blue, CRGB::Red, CRGB::Yellow);
+  // leds[1] = CRGB::HotPink;
+  // FastLED.show();
+  // delay(1000);
+
+  // fill_solid(leds, NUM_LEDS, CRGB::Red);
+  
+ 
+  
+
+
+for (int i = 0; i < NUM_LEDS; i++){
+  leds[i] = CRGB::Green;
+  leds[i-1] = CRGB::MediumBlue;
+  leds[i+1] = CRGB::MediumBlue;
+  FastLED.show();
+  delay(500);
+  leds[i] = CRGB::Black;
+  leds[i+1] = CRGB::Black;
+  leds[i-1] = CRGB::Black;
+  FastLED.show();
+  
+    
+  //  FastLED.show();
+}
+  
   // put your main code here, to run repeatedly:
 }
