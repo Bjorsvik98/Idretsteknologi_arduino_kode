d6d116b8bfb235d53aa949a44e6cb79acbc0032d
Andreas Bjørsvik <andreas.bjorsvik@gmail.com>
2022-03-02 08:33:57 +0100

    code working

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
-#include <Arduino.h>
-const int buzzer = 9; //buzzer to arduino pin 9
+// #include <Arduino.h>
+// const int buzzer = 9; //buzzer to arduino pin 9
 
-float array2[] = {3.23, 3.14, 3.24, 2.49, 3.25, 3.97};
-
-
+// float array2[] = {3.23, 3.14, 3.24, 2.49, 3.25, 3.97};
 
 
-void setup(){
-  Serial.begin(9600);
 
-}
 
-void loop(){
+// void setup(){
+//   Serial.begin(9600);
+
+// }
+
+// void loop(){
  
  
   
-}
+// }
