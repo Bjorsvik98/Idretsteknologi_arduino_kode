07fe5f8d08782f20abb74a1a6fe2bf02fc4d7c5a
Andreas Bjørsvik <andreas.bjorsvik@gmail.com>
2022-03-02 16:28:11 +0100

    done changes on LED strip

---
+++ src/OLED.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+
+void OLED_init() {
+
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        for(;;);
+    }
+    display.clearDisplay();
+    display.setTextColor(WHITE);
+    display.setTextSize(4);          // text size
+    // display.display();
+}
